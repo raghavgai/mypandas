b243d3e9638608dcc1ff733d3f94fe8f14289daf
raghavgai <raghavgaibond@gmail.com>
2019-06-14 09:19:12 +0530

    Stable my version of pandas 201906140919

--- asv_bench/benchmarks/offset.py
+++ asv_bench/benchmarks/offset.py
@@ -1,26 +1,117 @@
-:1554868886633,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: findChildElement","timestamp":1554868886685,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: getElementText","timestamp":1554868886732,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: findChildElement","timestamp":1554868886784,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: getElementText","timestamp":1554868886827,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: findChildElements","timestamp":1554868886878,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: findChildElement","timestamp":1554868886922,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: getElementText","timestamp":1554868886966,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: findChildElement","timestamp":1554868887019,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: getElementText","timestamp":1554868887153,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: findChildElement","timestamp":1554868887213,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: getElementText","timestamp":1554868887267,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: findChildElement","timestamp":1554868887320,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: getElementText","timestamp":1554868887362,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: findElements","timestamp":1554868887432,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: findChildElement","timestamp":1554868887477,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: getElementText","timestamp":1554868887522,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: findChildElement","timestamp":1554868887567,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: getElementText","timestamp":1554868887609,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: findChildElement","timestamp":1554868887737,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: getElementText","timestamp":1554868887785,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: findChildElement","timestamp":1554868887844,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: getElementText","timestamp":1554868887887,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: findChildElement","timestamp":1554868887964,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: getElementText","timestamp":1554868888011,"type":""}
-{"level":"INFO","message":"[fxdriver.nsCommandProcessor] Received command: findChildElement","timestamp":1554868888
+import warnings
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+try:
+    import pandas.tseries.holiday  # noqa
+except ImportError:
+    pass
+
+hcal = pd.tseries.holiday.USFederalHolidayCalendar()
+# These offests currently raise a NotImplimentedError with .apply_index()
+non_apply = [pd.offsets.Day(),
+             pd.offsets.BYearEnd(),
+             pd.offsets.BYearBegin(),
+             pd.offsets.BQuarterEnd(),
+             pd.offsets.BQuarterBegin(),
+             pd.offsets.BMonthEnd(),
+             pd.offsets.BMonthBegin(),
+             pd.offsets.CustomBusinessDay(),
+             pd.offsets.CustomBusinessDay(calendar=hcal),
+             pd.offsets.CustomBusinessMonthBegin(calendar=hcal),
+             pd.offsets.CustomBusinessMonthEnd(calendar=hcal),
+             pd.offsets.CustomBusinessMonthEnd(calendar=hcal)]
+other_offsets = [pd.offsets.YearEnd(), pd.offsets.YearBegin(),
+                 pd.offsets.QuarterEnd(), pd.offsets.QuarterBegin(),
+                 pd.offsets.MonthEnd(), pd.offsets.MonthBegin(),
+                 pd.offsets.DateOffset(months=2, days=2),
+                 pd.offsets.BusinessDay(), pd.offsets.SemiMonthEnd(),
+                 pd.offsets.SemiMonthBegin()]
+offsets = non_apply + other_offsets
+
+
+class ApplyIndex:
+
+    params = other_offsets
+    param_names = ['offset']
+
+    def setup(self, offset):
+        N = 10000
+        self.rng = pd.date_range(start='1/1/2000', periods=N, freq='T')
+
+    def time_apply_index(self, offset):
+        offset.apply_index(self.rng)
+
+
+class OnOffset:
+
+    params = offsets
+    param_names = ['offset']
+
+    def setup(self, offset):
+        self.dates = [datetime(2016, m, d)
+                      for m in [10, 11, 12]
+                      for d in [1, 2, 3, 28, 29, 30, 31]
+                      if not (m == 11 and d == 31)]
+
+    def time_on_offset(self, offset):
+        for date in self.dates:
+            offset.onOffset(date)
+
+
+class OffsetSeriesArithmetic:
+
+    params = offsets
+    param_names = ['offset']
+
+    def setup(self, offset):
+        N = 1000
+        rng = pd.date_range(start='1/1/2000', periods=N, freq='T')
+        self.data = pd.Series(rng)
+
+    def time_add_offset(self, offset):
+        with warnings.catch_warnings(record=True):
+            self.data + offset
+
+
+class OffsetDatetimeIndexArithmetic:
+
+    params = offsets
+    param_names = ['offset']
+
+    def setup(self, offset):
+        N = 1000
+        self.data = pd.date_range(start='1/1/2000', periods=N, freq='T')
+
+    def time_add_offset(self, offset):
+        with warnings.catch_warnings(record=True):
+            self.data + offset
+
+
+class OffestDatetimeArithmetic:
+
+    params = offsets
+    param_names = ['offset']
+
+    def setup(self, offset):
+        self.date = datetime(2011, 1, 1)
+        self.dt64 = np.datetime64('2011-01-01 09:00Z')
+
+    def time_apply(self, offset):
+        offset.apply(self.date)
+
+    def time_apply_np_dt64(self, offset):
+        offset.apply(self.dt64)
+
+    def time_add(self, offset):
+        self.date + offset
+
+    def time_add_10(self, offset):
+        self.date + (10 * offset)
+
+    def time_subtract(self, offset):
+        self.date - offset
+
+    def time_subtract_10(self, offset):
+        self.date - (10 * offset)
--- asv_bench/benchmarks/rolling.py
+++ asv_bench/benchmarks/rolling.py
@@ -1,32 +1,126 @@
-stamp":1554954772103,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"start\"}","timestamp":1554954772253,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"end\"}","timestamp":1554954772256,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"start\"}","timestamp":1554954772304,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"end\"}","timestamp":1554954772306,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"start\"}","timestamp":1554954772580,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"end\"}","timestamp":1554954772581,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"start\"}","timestamp":1554954772671,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"end\"}","timestamp":1554954772674,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"start\"}","timestamp":1554954772735,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"end\"}","timestamp":1554954772737,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"start\"}","timestamp":1554954772861,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"end\"}","timestamp":1554954772867,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"start\"}","timestamp":1554954773185,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"end\"}","timestamp":1554954773187,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"start\"}","timestamp":1554954773236,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"end\"}","timestamp":1554954773238,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"start\"}","timestamp":1554954773325,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"end\"}","timestamp":1554954773327,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"start\"}","timestamp":1554954773385,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"end\"}","timestamp":1554954773395,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"start\"}","timestamp":1554954773487,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"end\"}","timestamp":1554954773488,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"start\"}","timestamp":1554954773763,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"end\"}","timestamp":1554954773765,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"start\"}","timestamp":1554954773813,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"end\"}","timestamp":1554954773855,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"start\"}","timestamp":1554954773913,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"end\"}","timestamp":1554954773914,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"start\"}","timestamp":1554954773959,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"end\"}","timestamp":1554954774001,"type":"profiler"}
-{"level":"INFO","message":"{\"event\":\"YIELD_TO_PAGE_LOAD\",\"startorend\":\"start\"}","timesta
+import pandas as pd
+import numpy as np
+
+
+class Methods:
+
+    params = (['DataFrame', 'Series'],
+              [10, 1000],
+              ['int', 'float'],
+              ['median', 'mean', 'max', 'min', 'std', 'count', 'skew', 'kurt',
+               'sum'])
+    param_names = ['contructor', 'window', 'dtype', 'method']
+
+    def setup(self, constructor, window, dtype, method):
+        N = 10**5
+        arr = (100 * np.random.random(N)).astype(dtype)
+        self.roll = getattr(pd, constructor)(arr).rolling(window)
+
+    def time_rolling(self, constructor, window, dtype, method):
+        getattr(self.roll, method)()
+
+
+class ExpandingMethods:
+
+    params = (['DataFrame', 'Series'],
+              ['int', 'float'],
+              ['median', 'mean', 'max', 'min', 'std', 'count', 'skew', 'kurt',
+               'sum'])
+    param_names = ['contructor', 'window', 'dtype', 'method']
+
+    def setup(self, constructor, dtype, method):
+        N = 10**5
+        arr = (100 * np.random.random(N)).astype(dtype)
+        self.expanding = getattr(pd, constructor)(arr).expanding()
+
+    def time_expanding(self, constructor, dtype, method):
+        getattr(self.expanding, method)()
+
+
+class EWMMethods:
+
+    params = (['DataFrame', 'Series'],
+              [10, 1000],
+              ['int', 'float'],
+              ['mean', 'std'])
+    param_names = ['contructor', 'window', 'dtype', 'method']
+
+    def setup(self, constructor, window, dtype, method):
+        N = 10**5
+        arr = (100 * np.random.random(N)).astype(dtype)
+        self.ewm = getattr(pd, constructor)(arr).ewm(halflife=window)
+
+    def time_ewm(self, constructor, window, dtype, method):
+        getattr(self.ewm, method)()
+
+
+class VariableWindowMethods(Methods):
+    params = (['DataFrame', 'Series'],
+              ['50s', '1h', '1d'],
+              ['int', 'float'],
+              ['median', 'mean', 'max', 'min', 'std', 'count', 'skew', 'kurt',
+               'sum'])
+    param_names = ['contructor', 'window', 'dtype', 'method']
+
+    def setup(self, constructor, window, dtype, method):
+        N = 10**5
+        arr = (100 * np.random.random(N)).astype(dtype)
+        index = pd.date_range('2017-01-01', periods=N, freq='5s')
+        self.roll = getattr(pd, constructor)(arr, index=index).rolling(window)
+
+
+class Pairwise:
+
+    params = ([10, 1000, None],
+              ['corr', 'cov'],
+              [True, False])
+    param_names = ['window', 'method', 'pairwise']
+
+    def setup(self, window, method, pairwise):
+        N = 10**4
+        arr = np.random.random(N)
+        self.df = pd.DataFrame(arr)
+
+    def time_pairwise(self, window, method, pairwise):
+        if window is None:
+            r = self.df.expanding()
+        else:
+            r = self.df.rolling(window=window)
+        getattr(r, method)(self.df, pairwise=pairwise)
+
+
+class Quantile:
+    params = (['DataFrame', 'Series'],
+              [10, 1000],
+              ['int', 'float'],
+              [0, 0.5, 1],
+              ['linear', 'nearest', 'lower', 'higher', 'midpoint'])
+    param_names = ['constructor', 'window', 'dtype', 'percentile']
+
+    def setup(self, constructor, window, dtype, percentile, interpolation):
+        N = 10 ** 5
+        arr = np.random.random(N).astype(dtype)
+        self.roll = getattr(pd, constructor)(arr).rolling(window)
+
+    def time_quantile(self, constructor, window, dtype, percentile,
+                      interpolation):
+        self.roll.quantile(percentile, interpolation=interpolation)
+
+
+class PeakMemFixed:
+
+    def setup(self):
+        N = 10
+        arr = 100 * np.random.random(N)
+        self.roll = pd.Series(arr).rolling(10)
+
+    def peakmem_fixed(self):
+        # GH 25926
+        # This is to detect memory leaks in rolling operations.
+        # To save time this is only ran on one method.
+        # 6000 iterations is enough for most types of leaks to be detected
+        for x in range(6000):
+            self.roll.max()
+
+
+from .pandas_vb_common import setup  # noqa: F401
